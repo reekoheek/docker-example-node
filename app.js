f492765281128fdfd44b710719e21832ce91224e
Ganesha <reekoheek@gmail.com>
2020-03-05 19:03:12 +0700

    Implement for scale

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-  res.write('Hello world\n');
+  res.write(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello</title>
+</head>
+<body>
+  <h1>Hello</h1>
+  <p>${process.env.HOSTNAME}</p>
+</body>
+</html>
+  `.trim());
   res.end();
 }).listen(PORT);
